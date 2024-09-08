7c7051980436e9ad2d2b3e3a9f3f0d72f02d1817
MarcosRehtanz <marcos.rehtanz@gmail.com>
2024-09-07 23:52:41 -0300

    fix: remove solution

--- 06_usuario_mas_joven/index.js
+++ 06_usuario_mas_joven/index.js
@@ -3,17 +3,11 @@
  * 
  * Esta función encuentra y retorna el usuario más joven del arreglo proporcionado.
  * 
- * @param {Array} arr - Arreglo de objetos que representan usuarios.
+ * @param {Array} usuarios - Arreglo de objetos que representan usuarios.
  * @returns {Object|null} - Objeto del usuario más joven o null si el arreglo está vacío.
  */
-function encontrarUsuarioMasJoven(arr) {
-    if (arr.length === 0) {
-        return null; // Retorna null si el arreglo está vacío
-    }
-    
-    return arr.reduce((usuarioJoven, usuarioActual) => {
-        return (usuarioActual.edad < usuarioJoven.edad) ? usuarioActual : usuarioJoven;
-    });
+function encontrarUsuarioMasJoven(usuarios) {
+    // Tu código aquí
 }
 
 // Exportar la función para pruebas
