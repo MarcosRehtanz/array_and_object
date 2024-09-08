59f74649b054c2a807fe206503cfb4f0bff773b8
MarcosRehtanz <marcos.rehtanz@gmail.com>
2024-09-08 00:17:58 -0300

    fix: add example

--- 06_usuario_mas_joven/index.js
+++ 06_usuario_mas_joven/index.js
@@ -3,11 +3,23 @@
  * 
  * Esta función encuentra y retorna el usuario más joven del arreglo proporcionado.
  * 
- * @param {Array} usuarios - Arreglo de objetos que representan usuarios.
- * @returns {Object|null} - Objeto del usuario más joven o null si el arreglo está vacío.
+ * @example
+ * const usuarios = [
+ *    { nombre: "Juan", edad: 25 },
+ *    { nombre: "Ana", edad: 20 },
+ *    { nombre: "Pedro", edad: 30 },
+ *    { nombre: "Maria", edad: 15 }
+ * ];
+ * 
+ * encontrarUsuarioMasJoven(usuarios, "azul"); 
+ * // Debería retornar: { nombre: "Maria", edad: 15 }
  */
 function encontrarUsuarioMasJoven(usuarios) {
     // Tu código aquí
+    if (usuarios.length === 0) return null;
+    return usuarios.reduce((minor, user)=>{
+        return user.edad < minor.edad ? user : minor
+    }, usuarios[0])
 }
 
 // Exportar la función para pruebas
